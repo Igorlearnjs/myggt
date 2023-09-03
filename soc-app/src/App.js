fa3b6dbb54cc1b88728a3f3ab00bb3926be79af0
Igorlearnjs <igorkhotylev@Igors-MacBook-Pro.local>
2023-09-03 20:21:43 +0200

    Template

--- soc-app/src/App.js
+++ soc-app/src/App.js
@@ -4,11 +4,18 @@ import Header from './Header.js';
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://images.squarespace-cdn.com/content/v1/5be46505aa49a16a3b129522/1541695322945-8PXLVIS6XDKMKHLJOEIZ/Logo-NEW-WHITE.png?format=1500w" alt="Logo" className="logo" />
+                <a href="#">Home</a>
+            </header>
+            <nav className='navigation'>
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div className='content'>Main content</div>
         </div>
-    )
+    );
 }
 
 export default App;
